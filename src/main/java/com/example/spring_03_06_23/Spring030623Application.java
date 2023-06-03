05b392f3875a27cbac297943f6f7d545ef5ac9fc
kanva1984 <kanva1984@gmail.com>
2023-06-03 10:08:47 +0200

    начало дз spring

---
+++ src/main/java/com/example/spring_03_06_23/Spring030623Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_03_06_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring030623Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring030623Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_03_06_23/Spring030623ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_03_06_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring030623ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
